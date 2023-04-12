bfdddd2eff629c2770c4afa6bee70727ed623c68
AniModi <98632268+AniModi@users.noreply.github.com>
2023-04-12 16:15:24 +0530

    Added checkout page

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import './Checkout.scss';
+
+const Checkout = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [zip, setZip] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // handle form submission
+  };
+
+  return (
+    <div className="checkout-container">
+      <h2>Checkout</h2>
+      <form className="checkout-form" onSubmit={handleFormSubmit}>
+        <div className="checkout-input-group">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div className="checkout-input-group">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div className="checkout-input-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="checkout-input-group">
+          <label htmlFor="phone">Phone Number:</label>
+          <input
+            type="tel"
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+        <div className="checkout-input-group">
+          <label htmlFor="address">Address:</label>
+          <input
+            type="text"
+            id="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <div className="checkout-input-group">
+          <label htmlFor="city">City:</label>
+          <input
+            type="text"
+            id="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+        <div className="checkout-input-group">
+          <label htmlFor="state">State:</label>
+          <input
+            type="text"
+            id="state"
+            value={state}
+            onChange={(e) => setState(e.target.value)}
+          />
+        </div>
+        <div className="checkout-input-group">
+          <label htmlFor="zip">Zip Code:</label>
+          <input
+            type="text"
+            id="zip"
+            value={zip}
+            onChange={(e) => setZip(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn">
+          Place Order
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Checkout;
